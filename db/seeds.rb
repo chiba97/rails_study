06f67488a2c575b06ddc8bbac589b7034b295835
chiba97 <ktfkyrya@gmail.com>
2021-12-30 04:08:10 +0000

    [Add]question周り

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Question.create(id: 1, name: 'Test name1', title: 'Title Question1', content: 'Test content1')
+Question.create(id: 2, name: 'Test name2', title: 'Title Question2', content: 'Test content2')
+Question.create(id: 3, name: 'Test name3', title: 'Title Question3', content: 'Test content3')
